45e184a02df084cf53d967c6bd13e32bd4bb077d
patri <patrickmarkgraf@gmail.com>
2023-04-27 20:30:22 +0200

    make BookClient more resilient (timeout and retry)

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -1,8 +1,11 @@
 package com.polarbookshop.orderservice.book;
 
+import java.time.Duration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 @Component
 public class BookClient {
@@ -15,6 +18,14 @@ public class BookClient {
   }
 
   public Mono<Book> getBookByIsbn(String isbn) {
-    return webClient.get().uri(BOOKS_ROOT_API + isbn).retrieve().bodyToMono(Book.class);
+    return webClient
+        .get()
+        .uri(BOOKS_ROOT_API + isbn)
+        .retrieve()
+        .bodyToMono(Book.class)
+        .timeout(Duration.ofSeconds(3), Mono.empty())
+        .onErrorResume(WebClientResponseException.NotFound.class, exception -> Mono.empty())
+        .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+        .onErrorResume(Exception.class, exception -> Mono.empty());
   }
 }
