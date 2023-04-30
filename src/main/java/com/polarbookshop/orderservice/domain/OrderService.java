7634a962c74cdd6d47051317c2fdcfb5e2949761
patri <patrickmarkgraf@gmail.com>
2023-04-30 09:53:18 +0200

    add event producer

--- src/main/java/com/polarbookshop/orderservice/domain/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/domain/OrderService.java
@@ -2,16 +2,24 @@ package com.polarbookshop.orderservice.domain;
 
 import com.polarbookshop.orderservice.book.Book;
 import com.polarbookshop.orderservice.book.BookClient;
+import com.polarbookshop.orderservice.event.OrderAcceptedMessage;
 import com.polarbookshop.orderservice.event.OrderDispatchedMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class OrderService {
 
+  private static final Logger log = LoggerFactory.getLogger(OrderService.class);
+
   private final BookClient bookClient;
   private final OrderRepository orderRepository;
+  private final StreamBridge streamBridge;
 
   public static Order buildAcceptedOrder(Book book, int quantity) {
     return Order.of(
@@ -26,21 +34,25 @@ public class OrderService {
     return Order.of(bookIsbn, null, null, quantity, OrderStatus.REJECTED);
   }
 
-  public OrderService(BookClient bookClient, OrderRepository orderRepository) {
+  public OrderService(
+      BookClient bookClient, OrderRepository orderRepository, StreamBridge streamBridge) {
     this.bookClient = bookClient;
     this.orderRepository = orderRepository;
+    this.streamBridge = streamBridge;
   }
 
   public Flux<Order> getAllOrders() {
     return orderRepository.findAll();
   }
 
+  @Transactional
   public Mono<Order> submitOrder(String isbn, int quantity) {
     return bookClient
         .getBookByIsbn(isbn)
         .map(book -> buildAcceptedOrder(book, quantity))
         .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
-        .flatMap(orderRepository::save);
+        .flatMap(orderRepository::save)
+        .doOnNext(this::publishOrderAcceptedEvent);
   }
 
   public Flux<Order> consumeOrderDispatchedEvent(Flux<OrderDispatchedMessage> flux) {
@@ -60,5 +72,16 @@ public class OrderService {
         existingOrder.createdDate(),
         existingOrder.lastModifiedDate(),
         existingOrder.version());
+  }
+
+  private void publishOrderAcceptedEvent(Order order) {
+    if (!order.status().equals(OrderStatus.ACCEPTED)) {
+      return;
+    }
+
+    var orderAcceptedMessage = new OrderAcceptedMessage(order.id());
+    log.info("Sending order accepted event with id: {}", order.id());
+    var result = streamBridge.send("acceptedOrder-out-0", orderAcceptedMessage);
+    log.info("Result of sending data for order with id {}: {}", order.id(), result);
   }
 }
