1ef0647ed816fbc92be7d70b0eae8d5e71ced8d2
patri <patrickmarkgraf@gmail.com>
2023-05-08 22:57:35 +0200

    protect order api with Spring Security and OAuth2

--- src/test/java/com/polarbookshop/orderservice/web/OrderControllerWebFluxTests.java
+++ src/test/java/com/polarbookshop/orderservice/web/OrderControllerWebFluxTests.java
@@ -3,6 +3,7 @@ package com.polarbookshop.orderservice.web;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
+import com.polarbookshop.orderservice.config.SecurityConfig;
 import com.polarbookshop.orderservice.domain.Order;
 import com.polarbookshop.orderservice.domain.OrderService;
 import com.polarbookshop.orderservice.domain.OrderStatus;
@@ -10,15 +11,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
 @WebFluxTest(OrderController.class)
+@Import(SecurityConfig.class)
 class OrderControllerWebFluxTests {
 
   @Autowired private WebTestClient webClient;
 
   @MockBean private OrderService orderService;
+
+  @MockBean ReactiveJwtDecoder reactiveJwtDecoder;
 
   @Test
   void whenBookNotAvailableThenRejectOrder() {
@@ -30,6 +38,9 @@ class OrderControllerWebFluxTests {
         .willReturn(Mono.just(expectedOrder));
 
     webClient
+        .mutateWith(
+            SecurityMockServerConfigurers.mockJwt()
+                .authorities(new SimpleGrantedAuthority("ROLE_customer")))
         .post()
         .uri("/orders/")
         .bodyValue(orderRequest)
